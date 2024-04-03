373650af2f78ed629111289c4681ddf0f505a8c6
Pooja.Saini <pooja.saini@bridgeconn.com>
2024-04-03 12:36:41 +0530

    migrated from basic cra to nextjs

---
+++ src/pages/dictionary.js
@@ -0,0 +1,51 @@
+import React from "react";
+import axios from "axios";
+export default function Dictionary({ bookData, data_key }) {
+  const [dictData, setDictData] = React.useState();
+  const [dict, setDict] = React.useState([]);
+  const bookCode = bookData?.[0]?.code;
+  React.useEffect(() => {
+    const getDictData = async () => {
+      const response = await axios.get(
+        `/resources/search?resourceType=Dictionary&languageCode=eng&bookCode=${bookCode}&limit=100`,
+        {
+          headers: {
+            "api-key": data_key,
+          },
+        }
+      );
+      setDictData(response.data.items[0].id);
+    };
+    bookCode && data_key && getDictData();
+  }, [bookCode, data_key]);
+
+  React.useEffect(() => {
+    const dictSet = async () => {
+      const res = await axios.get(`/resources/${dictData}`, {
+        headers: {
+          "api-key": data_key,
+        },
+      });
+      setDict(res.data);
+    };
+
+    dictData && data_key && dictSet();
+  }, [dictData, data_key]);
+  return (
+    <div>
+      <h1>{dict.name}</h1>
+      <h2>{dict.localizedName}</h2>
+      <h3>Dictionary</h3>
+      {dict.content?.[0].tiptap.content.map((i) => {
+        if (i.type === "heading") {
+          return <h3>{i.content?.[0]?.text}</h3>;
+        }
+        if (i.type === "paragraph") {
+          return i.content.map((e) => {
+            return e.text;
+          });
+        }
+      })}
+    </div>
+  );
+}
